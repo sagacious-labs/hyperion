7b9b3a5a8487a4f3061ec3d8490ac7296649e19c
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-09-17 02:18:11 +0530

    reflect proto change in the build script
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- build.rs
+++ build.rs
@@ -1,10 +1,9 @@
 fn main() {
-	let file = "protos/ops.proto";
+    let file = "protos/api/v1/api.proto";
 
-	println!("Compiling {}...", file);
-    tonic_build::compile_protos(file)
-        .unwrap_or_else(|e| panic!("Failed to compile proto {:?}", e));
-	println!("Completed");
-	
-	println!("cargo:rerun-if-changed={}", file);
+    println!("Compiling {}...", file);
+    tonic_build::compile_protos(file).unwrap_or_else(|e| panic!("Failed to compile proto {:?}", e));
+    println!("Completed");
+
+    println!("cargo:rerun-if-changed={}", file);
 }
