a606b9fe487a391d7fdac6a2fbab4e2df3a911d8
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-11-03 04:27:51 +0530

    integrate manager and gRPC API
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 #![allow(dead_code, unused_variables)]
+
+use actor::Actor;
 mod actor;
 mod proto;
+mod server;
 mod utility;
 mod woduler;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create woduler manager
+    let manager = woduler::manager::Manager::new();
+    // Start the manager actor
+    let manager_mailbox = manager.start();
+
+    // Create api server
+    server::start(server::Config {
+        host: "0.0.0.0",
+        port: "2310",
+        mailbox: manager_mailbox,
+    })
+    .await?;
+
     Ok(())
 }
