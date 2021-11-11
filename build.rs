d32269588ed774a9785aa4a374ce0b72896f1bec
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-11-11 16:12:39 +0530

    improve proto build process for relative imports
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- build.rs
+++ build.rs
@@ -1,9 +1,6 @@
 fn main() {
     let files = ["protos/v1alpha1/api/api.proto"];
-
-    println!("Compiling {:?}...", files);
-    tonic_build::configure().compile(&files, &["."]).unwrap();
-    println!("Completed");
-
-    // println!("cargo:rerun-if-changed=protos");
+    tonic_build::configure()
+        .compile(&files, &[".", "protos"])
+        .unwrap();
 }
